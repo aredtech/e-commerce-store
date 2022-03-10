68b0f86aa9af98e356f5c37ca91a2a983e00f278
Raju Mandal <raju.subharth.pay07@gmail.com>
2022-03-10 20:37:56 +0530

    Change db to mysql

--- e_commerce_app_project/e_commerce_app_project/settings.py
+++ e_commerce_app_project/e_commerce_app_project/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'e_commerce_app_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'e_commerce_store',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
 
