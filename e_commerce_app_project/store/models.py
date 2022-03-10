db3cec00a6f0e10da72d1e7d5c55231bba53a086
Raju Mandal <raju.subharth.pay07@gmail.com>
2022-03-10 21:17:35 +0530

    Add Customer, Address, Order, OrderItem, Cart, CartItem model

--- e_commerce_app_project/store/models.py
+++ e_commerce_app_project/store/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(0.1)])
+    unit_price = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(0.1)])
     description = models.TextField()
     stock = models.IntegerField(validators=[MinValueValidator(1)])
     slug = models.SlugField()
-    last_update = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey("Collection", on_delete=models.PROTECT)
     product = models.ManyToManyField("Promotion", blank=True)
 
@@ -18,15 +21,18 @@ class Product(models.Model):
     class Meta:
         ordering = ["title"]
 
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    featured_product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True, related_name="+")
 
     def __str__(self):
         return self.title
 
     class Meta:
         ordering = ["title"]
+
 
 class Promotion(models.Model):
     title = models.CharField(max_length=255)
@@ -39,4 +45,68 @@ class Promotion(models.Model):
         ordering = ["title"]
 
 
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
 
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, "Bronze"),
+        (MEMBERSHIP_SILVER, "Silver"),
+        (MEMBERSHIP_GOLD, "G"),
+    ]
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    birth_date = models.DateField()
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+
+    PAYMENT_PENDING = "P"
+    PAYMENT_FAILED = "F"
+    PAYMENT_SUCCESS = "S"
+
+    PAYMENT_CHOICES = [
+        (PAYMENT_PENDING, "Pending"),
+        (PAYMENT_FAILED, "Failed"),
+        (PAYMENT_SUCCESS, "Success"),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(
+        max_digits=6, decimal_places=2, validators=[MinValueValidator(0.1)])
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
